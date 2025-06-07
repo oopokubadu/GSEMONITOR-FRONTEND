9780be9ac51c9c7b2555f208bd430b465d4e4b10
Victor <69175873+BrumNet@users.noreply.github.com>
2025-06-07 05:15:23 +0000

    update local storage

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -6,9 +6,11 @@ export default function Callback() {
 
   useEffect(() => {
       const urlParams = new URLSearchParams(window.location.search);
-      const authToken = urlParams.get("auth_token");
       const userId = urlParams.get("user_id");
-      window.location.href = "/dashboard?user_id=" + userId;
+      const authToken = urlParams.get("auth_token");
+      localStorage.setItem("authToken", authToken || ""); 
+      localStorage.setItem("userId", userId || "");
+      window.location.href = "/dashboard"; // Redirect to the dashboard after setting local storage
   }, []); // Redirect to the dashboard when the component mounts
 
   return <p>Signing you in...</p>;
