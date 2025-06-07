9ad5102bd7d92e680d6372e7458cbc1ea2656d6c
Victor <69175873+BrumNet@users.noreply.github.com>
2025-06-07 04:59:19 +0000

    test fix

--- hooks/use-get-profile.tsx
+++ hooks/use-get-profile.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 // Function to fetch user profile
@@ -31,10 +33,15 @@ async function fetchUserProfile() {
 
 // Hook to use user profile data
 export function useGetProfile() {
-  const urlParams = new URLSearchParams(window.location.search);
+  const urlParams = typeof window != "undefined" && new URLSearchParams(window.location.search);
 
   return useQuery({
-    queryKey: ["userProfile", urlParams.get("user_id") ?? localStorage.getItem("userId") ], // Unique query key for caching
+    queryKey: [
+      "userProfile",
+      urlParams && typeof urlParams.get === "function"
+        ? urlParams.get("user_id")
+        : localStorage.getItem("userId")
+    ], // Unique query key for caching
     queryFn: fetchUserProfile, // Fetch user profile using userId
     enabled: !!localStorage.getItem("userId"), // Only fetch if userId exists in local storage
     staleTime: 5 * 60 * 1000, // Consider data fresh for 5 minutes
