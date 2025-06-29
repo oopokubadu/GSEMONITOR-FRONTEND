2374390270655c2fc22d2a80a28e688fb6244151
Victor <69175873+BrumNet@users.noreply.github.com>
2025-06-29 11:23:29 +0000

    chore remove comment

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,10 +4,6 @@ import { PortfolioContent } from "@/components/portfolio-content"
 export default function PortfolioPage() {
   return (
     <DashboardLayout>
-      {/* <div className="flex flex-col space-y-4 p-4 lg:p-6">
-        <h1 className="text-2xl font-bold">Portfolio</h1>
-        <p className="text-muted-foreground">Coming Soon...</p>
-      </div> */}
       <PortfolioContent />
     </DashboardLayout>
   )
